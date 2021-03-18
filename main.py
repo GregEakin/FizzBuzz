3d4210d0a6c673580780df624c3977ba3a22d416
Gregory Eakin <greg@eakin.dev>
2021-03-18 15:23:22 -0700

    Remove the old branch logic.

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 # This is a sample Python script.
 
 def print_num(number):
-    if number % 15 == 0:
-        return "FizzBuzz"
-    elif number % 5 == 0:
-        return 'Buzz'
-    elif number % 3 == 0:
-        return 'Fizz'
-    else:
-        return f'{number}'
+    return f'{number}'
